03871b1115241260b716691624042adc2ea5771a
Robertarezza <roberta.rezza@gmail.com>
2024-05-27 10:16:17 +0200

    add: bonus 2

--- js/script.js
+++ js/script.js
@@ -4,18 +4,23 @@ createApp({
   data() {
     return {
       dischi: [],
+      pref: [],
       apiUrl: "http://localhost/boolean/php-dischi-json/server.php",
     };
   },
   created() {
-    axios
-      .get(this.apiUrl)
-      .then((resp) => {
-        this.dischi = resp.data.result;
-        console.log(this.dischi);
-      });
+    this.fetchDischi();
   },
   methods: {
+    fetchDischi() {
+      axios
+        .get(this.apiUrl)
+        .then((resp) => {
+          this.dischi = resp.data.result;
+          console.log(this.dischi);
+        });
+    },
+
     taggleLike(index) {
       const data = {
         action: "toggle-like",
@@ -31,6 +36,24 @@ createApp({
         this.dischi = resp.data.result;
           // console.log(resp);
         });
+    },
+    addFilter() {
+      if ( this.pref.length >= 1 ) {
+        axios
+          .get(this.apiUrl, {
+            params: {
+              like: true,
+            },
+          })
+          .then((resp) => {
+            console.log(resp);
+              this.dischi = resp.data.result;
+              console.log(this.dischi);
+            
+          });
+      }  else {
+        this.fetchDischi();
+      }
     },
   },
 }).mount("#app");
--- server.php
+++ server.php
@@ -13,15 +13,37 @@ $dischi_list = json_decode($list_dischi_string, true);
 
 
 if (isset($_POST["action"]) && $_POST["action"] === "toggle-like") {
-    
+
     $disc_index = $_POST["disc_index"];
 
     $dischi_list[$disc_index]["like"] = !$dischi_list[$disc_index]["like"];
     file_put_contents("dischi.json", json_encode($dischi_list));
-    
+
+} elseif (isset($_GET["like"]) && $_GET["like"] === "true") {
+    // Filtrare i dischi che hanno "like" impostato a true
+    $liked_discs = array_filter($dischi_list, function ($cur_disk) {
+        return isset($cur_disk["like"]) && $cur_disk["like"];
+    });
+
+    // Restituire i dischi filtrati come risposta
+
+    // Preparazione della risposta
+    $dischi = [
+        "result" => array_values($liked_discs)
+    ];
+
+    // Conversione in formato JSON
+    $json_list_dischi = json_encode($dischi); 
+
+    // Comunicazione che si sta inviando un file JSON
+    header("Content-Type: application/json");
+
+    // Invio della risposta
+    echo $json_list_dischi;
+
+    // Terminazione dello script
+    exit();
 }
-
-
 
 
 
